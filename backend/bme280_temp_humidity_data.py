37d492b77b7b6152aed8c70a6712b4cc2acea541
mcyph <20507948+mcyph@users.noreply.github.com>
2021-04-04 22:32:55 +1000

    started adding fastapi-based api

--- backend/bme280_temp_humidity_data.py
+++ backend/bme280_temp_humidity_data.py
@@ -20,10 +20,10 @@ bme280.sea_level_pressure = 1013.25
 
 
 def get_values():
-    print("\nTemperature: %0.1f C" % bme280.temperature)
-    print("Humidity: %0.1f %%" % bme280.relative_humidity)
-    print("Pressure: %0.1f hPa" % bme280.pressure)
-    print("Altitude = %0.2f meters" % bme280.altitude)
+    #print("\nTemperature: %0.1f C" % bme280.temperature)
+    #print("Humidity: %0.1f %%" % bme280.relative_humidity)
+    #print("Pressure: %0.1f hPa" % bme280.pressure)
+    #print("Altitude = %0.2f meters" % bme280.altitude)
 
     return [
         ['temp', bme280.temperature],
