54dc0ecb7aa87d9b51194a9707e3c1c01a3e540a
mcyph <20507948+mcyph@users.noreply.github.com>
2021-04-06 17:33:34 +1000

    changed to ws: websockets protocol

--- backend/api.py
+++ backend/api.py
@@ -10,14 +10,14 @@ app = FastAPI()
 lock = allocate_lock()
 queues = {}
 
-#origins = [
-#    "http://localhost",
-#    "http://localhost:3000",
-#]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
